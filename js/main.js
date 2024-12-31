e7519ae56bc9c332dfe3b9d9a33c6213a8102214
Mohamed-Gabal <abogabal672@gmail.com>
2024-12-31 07:59:17 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+//open close cart
+var cart = document.querySelector('.cart');
+
+function open_cart() {
+    cart.classList.add("active")
+}
+function close_cart() {
+    cart.classList.remove("active")
+}
+
+// add items in cart
---
+++ js/swiper.js
@@ -0,0 +1,26 @@
+// Swiper Slide Sidebar
+var swiper = new Swiper(".slide-swp", {
+    pagination: {
+      el: ".swiper-pagination",
+      DynamicBullests: true,
+      clickable: true,
+    },
+    autoplay:{
+      delay:1000,
+    },
+    loop:true,
+  });
+// // Swiper Slide Slade-slide
+var swiper = new Swiper(".sale-sec", {
+    pagination: {
+      el: ".swiper-pagination",
+      DynamicBullests: true,
+      clickable: true,
+    },
+    slidesPerView: 4,
+    spaceBetween: 30,
+    autoplay:{
+      delay:1000,
+    },
+    loop:true,
+  });
